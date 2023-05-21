51a03c82dc034401c667da4cca80b40771992f7b
Michael Montagnese <mmont429@gmail.com>
2023-05-21 18:00:38 +0100

    Fix time when starting

--- Assets/Scripts/SceneNavigation/StartMenu.cs
+++ Assets/Scripts/SceneNavigation/StartMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class StartMenu : MonoBehaviour
 {
     public void StartClick() {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(1);
     }
 
