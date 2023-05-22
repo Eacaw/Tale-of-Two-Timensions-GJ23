8929cddb7684a09d501e9e978dc03938c763c2ec
Michael Montagnese <mmont429@gmail.com>
2023-05-22 18:26:34 +0100

    Destroy player when going back to main menu

--- Assets/Scripts/SceneNavigation/EndMenu.cs
+++ Assets/Scripts/SceneNavigation/EndMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class EndMenu : MonoBehaviour
 {
     public void ReturnToMain() {
+        Destroy(GameObject.FindGameObjectsWithTag("Player")[0]);
         SceneManager.LoadScene(0);
     }
 
