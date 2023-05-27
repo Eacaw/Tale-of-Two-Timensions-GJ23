0078f53ba3322bd7d59be7859fcceba666cd9f92
Eacaw <davidpinchen@gmail.com>
2023-05-27 17:04:58 +0100

    Add dying animation to blacksmith

--- Assets/Scripts/NPC/BlacksmithDialog.cs
+++ Assets/Scripts/NPC/BlacksmithDialog.cs
@@ -29,17 +29,21 @@ public class BlacksmithDialog : MonoBehaviour
 
     void OnMouseDown()
     {
-        if (
-            GameObject.FindGameObjectsWithTag("Player")[0]
-                .GetComponent<PlayerController>()
-                .currentCheckpoint < 6
-        )
+        PlayerController playerController = GameObject.FindGameObjectsWithTag("Player")[
+            0
+        ].GetComponent<PlayerController>();
+
+        if (playerController.currentCheckpoint < 6)
         {
             dialogItems[0].TriggerDialogue();
         }
-        else
+        else if (playerController.currentCheckpoint < 8)
         {
             dialogItems[1].TriggerDialogue();
+            GameObject.FindGameObjectsWithTag("Blacksmith")[0]
+                .GetComponent<Animator>()
+                .SetTrigger("isDying");
+            playerController.currentCheckpoint = 8;
         }
     }
 
