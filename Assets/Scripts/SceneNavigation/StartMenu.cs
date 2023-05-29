b7a35472b6de3abb9b1187f055693879071f0443
Lily Clark <lilykateclark21@gmail.com>
2023-05-29 11:52:29 +0100

    Update scripts with new sounds

--- Assets/Scripts/SceneNavigation/StartMenu.cs
+++ Assets/Scripts/SceneNavigation/StartMenu.cs
@@ -5,17 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
+    [SerializeField] private FMODUnity.EventReference ClickEventPath;
+
     public void StartClick() {
+        PlayButtonSound();
+
         Time.timeScale = 1f;
         SceneManager.LoadScene(4);
         GameObject player = GameObject.Find("Player");
         PlayerController playerController = player.GetComponent<PlayerController>();
         playerController.textMesh.gameObject.SetActive(true);
-         playerController.yearIndicator.gameObject.SetActive(true);
+        playerController.yearIndicator.gameObject.SetActive(true);
         playerController.yearIndicatorBackground.gameObject.SetActive(true);
     }
 
     public void QuitGame() {
+        PlayButtonSound();
+
         Application.Quit();
+    }
+
+    void PlayButtonSound()
+    {
+        FMOD.Studio.EventInstance click = FMODUnity.RuntimeManager.CreateInstance(ClickEventPath);
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(click, transform, GetComponent<Rigidbody>());
+
+        click.start();
+        click.release();
     }
 }
